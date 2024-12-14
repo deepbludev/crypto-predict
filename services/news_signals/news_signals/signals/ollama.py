92c63b40b73ade6a7ad2b73305002dfba75bb421
Carlo Casorzo <carlo@deepblu.dev>
2024-12-14 19:26:46 +0100

    fix: use interpolated vars in prompt

--- services/news_signals/news_signals/signals/ollama.py
+++ services/news_signals/news_signals/signals/ollama.py
@@ -32,7 +32,22 @@ class OllamaSentimentAnalyzer(SentimentAnalyzer):
 
     def __init__(self, llm_model: LLMModel, base_url: str):
         self.llm = Ollama(model=llm_model, base_url=base_url, temperature=0)
-        self.prompt_template = PromptTemplate(template=dedent(template).strip())
+
+        assets = ", ".join(a.value for a in Asset)
+        signals = ", ".join(s.value for s in SentimentSignal)
+        prompt = f"""
+            You are an expert crypto financial analyst.
+            
+            You are given a news story and you need to provide a sentiment signal
+            based on the impact of the news on the prices of the following crypto assets: {assets}.
+            
+            The result of the sentiment analysis for each asset should be one of the following: {signals}.
+
+            Here is the news story:
+            {{news_story}}
+        """  # noqa
+
+        self.prompt_template = PromptTemplate(template=dedent(prompt).strip())
 
         super().__init__(llm_model)
 
