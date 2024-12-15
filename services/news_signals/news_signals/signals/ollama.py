8fd6749ab4b49a7cd800df11d5d9d88ee751970a
Carlo Casorzo <carlo@deepblu.dev>
2024-12-15 11:37:51 +0100

    feat: add temp option to ollama

--- services/news_signals/news_signals/signals/ollama.py
+++ services/news_signals/news_signals/signals/ollama.py
@@ -8,12 +8,12 @@ from .analyzer import SentimentAnalyzer
 class OllamaSentimentAnalyzer(SentimentAnalyzer):
     """Sentiment analyzer using Ollama."""
 
-    def __init__(self, llm_model: LLMModel, base_url: str):
+    def __init__(self, llm_model: LLMModel, base_url: str, temperature: float = 0):
         super().__init__(
             llm_model,
             llm=Ollama(
                 model=llm_model,
                 base_url=base_url,
-                temperature=0,
+                temperature=temperature,
             ),
         )
