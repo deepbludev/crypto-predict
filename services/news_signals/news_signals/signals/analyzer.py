9766939d8998df814b59178f3d1cc87ba71f3781
Carlo Casorzo <carlo@deepblu.dev>
2024-12-15 10:58:19 +0100

    refactor: improve abstraction for LLMs

--- services/news_signals/news_signals/signals/analyzer.py
+++ services/news_signals/news_signals/signals/analyzer.py
@@ -1,15 +1,53 @@
-from abc import ABC, abstractmethod
+from textwrap import dedent
+
+from llama_index.core.llms import LLM
+from llama_index.core.prompts import PromptTemplate
 
 from domain.llm import LLMModel
 from domain.news import NewsStory
-from domain.sentiment_analysis import SentimentAnalysisResult
+from domain.sentiment_analysis import (
+    NewsStorySentimentAnalysis,
+    SentimentAnalysisResult,
+    SentimentSignal,
+)
+from domain.trades import Asset
+
+assets = ", ".join(a.value for a in Asset)
+signals = ", ".join(s.value for s in SentimentSignal)
+prompt = f"""
+    You are an expert crypto financial analyst.
+    
+    You are given a news story and you need to provide a sentiment signal
+    based on the impact of the news on the prices of the following crypto assets: {assets}.
+    
+    The result of the sentiment analysis for each asset should be one of the following: {signals}.
+
+    Here is the news story:
+    {{news_story}}
+"""  # noqa
 
 
-class SentimentAnalyzer(ABC):
-    def __init__(self, llm_model: LLMModel):
+class SentimentAnalyzer:
+    def __init__(self, llm_model: LLMModel, llm: LLM):
+        self.llm = llm
         self.llm_model = llm_model
+        self.prompt_template = PromptTemplate(template=dedent(prompt).strip())
 
-    @abstractmethod
-    def analyze(self, story: NewsStory) -> SentimentAnalysisResult:
-        """Analyze the sentiment of the given news story."""
-        ...
+    def analyze(self, story: NewsStory) -> NewsStorySentimentAnalysis:
+        """
+        Analyze the sentiment of the given news story using the LLM model
+        and the prompt template.
+
+        Returns:
+            NewsStorySentimentAnalysis: The sentiment analysis result.
+        """
+        result = self.llm.structured_predict(
+            SentimentAnalysisResult,
+            prompt=self.prompt_template,
+            news_story=story.title,
+        )
+        return NewsStorySentimentAnalysis(
+            llm_model=self.llm_model,
+            story=story.title,
+            **result.model_dump(),
+        )
--- services/news_signals/news_signals/signals/ollama.py
+++ services/news_signals/news_signals/signals/ollama.py
@@ -1,16 +1,6 @@
-from textwrap import dedent
-
-from llama_index.core.prompts import PromptTemplate
 from llama_index.llms.ollama import Ollama
 
 from domain.llm import LLMModel
-from domain.news import NewsStory
-from domain.sentiment_analysis import (
-    NewsStorySentimentAnalysis,
-    SentimentAnalysisResult,
-    SentimentSignal,
-)
-from domain.trades import Asset
 
 from .analyzer import SentimentAnalyzer
 
@@ -19,32 +9,11 @@ class OllamaSentimentAnalyzer(SentimentAnalyzer):
     """Sentiment analyzer using Ollama."""
 
     def __init__(self, llm_model: LLMModel, base_url: str):
-        self.llm = Ollama(model=llm_model, base_url=base_url, temperature=0)
-
-        assets = ", ".join(a.value for a in Asset)
-        signals = ", ".join(s.value for s in SentimentSignal)
-        prompt = f"""
-            You are an expert crypto financial analyst.
-            
-            You are given a news story and you need to provide a sentiment signal
-            based on the impact of the news on the prices of the following crypto assets: {assets}.
-            
-            The result of the sentiment analysis for each asset should be one of the following: {signals}.
-
-            Here is the news story:
-            {{news_story}}
-        """  # noqa
-
-        self.prompt_template = PromptTemplate(template=dedent(prompt).strip())
-
-        super().__init__(llm_model)
-
-    def analyze(self, story: NewsStory) -> NewsStorySentimentAnalysis:
-        result = self.llm.structured_predict(
-            SentimentAnalysisResult, prompt=self.prompt_template, news_story=story.title
-        )
-        return NewsStorySentimentAnalysis(
-            story=story.title,
-            llm_model=self.llm_model,
-            **result.model_dump(),
+        super().__init__(
+            llm_model,
+            llm=Ollama(
+                model=llm_model,
+                base_url=base_url,
+                temperature=0,
+            ),
         )
